b8d406fabb2e6e9ab8159375df0e391c717b70f1
Yukun Xie <yxie@lcs.on.ca>
2024-09-26 11:32:12 -0400

    Made the first draft of the input function and the main functino to calculation

--- Logic and Unit Tests 2/main.swift
+++ Logic and Unit Tests 2/main.swift
@@ -7,5 +7,108 @@
 
 import Foundation
 
-print("Hello, World!")
+func getInputN(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+    
+    // Loop until a valid value is provided
+    while true {
+        
+        // Prompt the user
+        print(prompt)
+        
+        // Collect the input
+        guard let givenInput = readLine() else {
+            continue
+        }
+        
+        // Convert to an integer
+        guard let givenInteger = Int(givenInput) else {
+            continue
+        }
+        
+        // If a lowest value for the integer was specified...
+        if let minimumValue = minimum {
+            
+            // ... then check that the given integer is greater than or equal to the lowest desired value.
+            guard givenInteger >= minimumValue else {
+                continue
+            }
+            
+        }
+        
+        // If an highest possible value for the integer was specified...
+        if let maximumValue = maximum {
+            
+            // ... then check that the given integer is less than or equal to the highest desired value.
+            guard givenInteger <= maximumValue else {
+                continue
+            }
+            
+            
+        }
+        
+        // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
+        return givenInteger
+        
+    }
+    
+}
+func getInputK(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+    
+    // Loop until a valid value is provided
+    while true {
+        
+        // Prompt the user
+        print(prompt)
+        
+        // Collect the input
+        guard let givenInput = readLine() else {
+            continue
+        }
+        
+        // Convert to an integer
+        guard let givenInteger = Int(givenInput) else {
+            continue
+        }
+        
+        // If a lowest value for the integer was specified...
+        if let minimumValue = minimum {
+            
+            // ... then check that the given integer is greater than or equal to the lowest desired value.
+            guard givenInteger >= minimumValue else {
+                continue
+            }
+            
+        }
+        
+        // If an highest possible value for the integer was specified...
+        if let maximumValue = maximum {
+            
+            // ... then check that the given integer is less than or equal to the highest desired value.
+            guard givenInteger <= maximumValue else {
+                continue
+            }
+            
+            
+        }
+        
+        // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
+        return givenInteger
+        
+    }
+    
+}
 
+var InputN = getInputN(withPrompt: "Input N", minimum: 1, maximum: 10000)
+var InputK = getInputN(withPrompt: "Input K", minimum: 0, maximum: 5)
+
+func getSum(n: Int, k: Int) -> Int {
+    var sum = 0
+    var s = 0
+    
+    for i in 0...k {
+        s = n+10^k
+    }
+    
+    sum+=s
+     return sum
+}
